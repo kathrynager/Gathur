a674f79d7e0fbccb4f3523ebab7ac8ecf2e7b6b2
Jiqing Xu <jiqingxu@dyn-72-33-214-70.uwnet.wisc.edu>
2015-11-15 11:53:04 -0600

    Initial Commit

---
+++ Gathur/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gathur
+//
+//  Created by Jiqing Xu on 11/15/15.
+//  Copyright © 2015 Jiqing Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
